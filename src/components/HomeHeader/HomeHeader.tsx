28f5cf8eb40302f556ba0276f2da56270ad27415
Ricardo Baldrez <rdbz@gft.com>
2025-08-05 22:43:24 -0300

    chore: Reusing types

--- src/components/HomeHeader/HomeHeader.tsx
+++ src/components/HomeHeader/HomeHeader.tsx
@@ -5,10 +5,12 @@ import { styles } from "./HomeHeaderStyles";
 import { colors } from "@/theme/colors";
 
 import { Separator } from "@/components/Separator";
-import { Summary } from "@/components/Summary";
+import { Summary, SummaryProps } from "@/components/Summary";
 
 export type HomeHeaderProps = {
   total: string;
+  input: SummaryProps;
+  output: SummaryProps;
 };
 
 type Props = {
@@ -30,12 +32,12 @@ export function HomeHeader({ data }: Props) {
 
       <View style={styles.summary}>
         <Summary
-          data={{ label: "Entradas", value: "R$ 6.185,90" }}
+          data={data.input}
           icon={{ name: "arrow-upward", color: colors.green[500] }}
         />
         <Summary
           isLeft
-          data={{ label: "Saídas", value: "-R$ 883,65" }}
+          data={data.output}
           icon={{ name: "arrow-downward", color: colors.red[400] }}
         />
       </View>
