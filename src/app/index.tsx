411b84a97fb779b22794f15432ad0632ec533b20
Ricardo Baldrez <rdbz@gft.com>
2025-08-05 22:59:51 -0300

    chore: Add Target component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import { View } from "react-native";
 
+import { Target } from "@/components/Target/Target";
 import { HomeHeader } from "@/components/HomeHeader/HomeHeader";
 
 const summary = {
@@ -12,12 +13,23 @@ const summary = {
     label: "Saídas",
     value: "-R$ 883,65",
   },
-}
+};
+
+const targets = [
+  {
+    id: "1",
+    name: "Apple Watch Series 7",
+    percentage: "75%",
+    current: "R$ 1.340,00",
+    target: "R$ 3.280,00",
+  },
+];
 
 export default function Index() {
   return (
-    <View style={{ flex: 1}}>
+    <View style={{ flex: 1 }}>
       <HomeHeader data={summary} />
+      <Target data={targets[0]} />
     </View>
-  )
+  );
 }
---
+++ src/components/Target/Target.tsx
@@ -0,0 +1,38 @@
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { styles } from "./TargetStyles";
+
+export type TargetProps = {
+  id: string
+  name: string;
+  percentage: string;
+  current: string;
+  target: string;
+}
+
+type Props = TouchableOpacityProps & {
+  data: TargetProps;
+};
+
+export function Target({ data, ...rest }: Props) {
+  return (
+    <TouchableOpacity {...rest} style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.name} numberOfLines={1}>
+          {data.name}
+        </Text>
+        <Text style={styles.status}>
+          {data.percentage} • {data.current} de {data.target}
+        </Text>
+      </View>
+
+      <MaterialIcons name="chevron-right" size={20} />
+    </TouchableOpacity>
+  )
+}
