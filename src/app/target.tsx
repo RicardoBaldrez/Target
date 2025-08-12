49311065385ca200410c64960a6a95cadb628db5
Ricardo Baldrez <rdbz@gft.com>
2025-08-12 18:43:21 -0300

    feat: Add CurrencyInput component

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,9 +1,9 @@
 import { View } from "react-native";
-import { router } from "expo-router";
 
 import { PageHeader } from "@/components/PageHeader";
 import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
+import { CurrencyInput } from "@/components/CurrencyInput";
 
 export default function Target() {
   return (
@@ -14,6 +14,7 @@ export default function Target() {
       />
       <View style={{ marginTop: 32, gap: 24 }}>
         <Input label="Nome da meta" placeholder="Ex: Viagem para a praia, Apple Watch" />
+        <CurrencyInput label="Valor da alvo" value={0} />
         <Button title="Salvar" />
       </View>
     </View>
