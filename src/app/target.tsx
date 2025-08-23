fa8f9a1cb4e05d83ebffaa6231c664948bd5dcd8
Ricardo Baldrez <rdbz@gft.com>
2025-08-23 17:32:38 -0300

    feat: Add handleSave() function

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,4 +1,8 @@
-import { View } from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable no-undef */
+import { router, useLocalSearchParams } from 'expo-router';
+import { useState } from 'react';
+import { Alert, View } from 'react-native';
 
 import { Button } from '@/components/Button';
 import { CurrencyInput } from '@/components/CurrencyInput';
@@ -6,6 +10,40 @@ import { Input } from '@/components/Input';
 import { PageHeader } from '@/components/PageHeader';
 
 export default function Target() {
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [name, setName] = useState('');
+  const [amount, setAmount] = useState(0);
+  const params = useLocalSearchParams<{ id?: string }>();
+
+  function handleSave() {
+    if (!name.trim() || amount <= 0) {
+      return Alert.alert('Atenção', 'Preencha todos os campos');
+    }
+
+    setIsProcessing(true);
+
+    if (params.id) {
+      console.log('update');
+    } else {
+      handleCreateTarget();
+    }
+  }
+
+  async function handleCreateTarget() {
+    try {
+      Alert.alert('Sucesso', 'Meta criada com sucesso!', [
+        {
+          text: 'Ok',
+          onPress: () => router.back(),
+        },
+      ]);
+    } catch (error) {
+      Alert.alert('Erro', 'Erro ao criar meta.');
+      console.log('🚀 ~ handleCreateTarget ~ error:', error);
+      setIsProcessing(false);
+    }
+  }
+
   return (
     <View style={{ flex: 1, padding: 24 }}>
       <PageHeader
@@ -16,9 +54,19 @@ export default function Target() {
         <Input
           label="Nome da meta"
           placeholder="Ex: Viagem para a praia, Apple Watch"
+          value={name}
+          onChangeText={setName}
         />
-        <CurrencyInput label="Valor da alvo (R$)" value={0} />
-        <Button title="Salvar" />
+        <CurrencyInput
+          label="Valor da alvo (R$)"
+          value={amount}
+          onChangeValue={setAmount}
+        />
+        <Button
+          title="Salvar"
+          onPress={handleSave}
+          isProcessing={isProcessing}
+        />
       </View>
     </View>
   );
