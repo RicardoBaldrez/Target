27340d38bf78ca0e84745ea3fa97b57177068d1f
Ricardo Baldrez <rdbz@gft.com>
2025-08-24 16:44:13 -0300

    feat: Add list targets function

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,13 @@
-import { router } from 'expo-router';
-import { StatusBar, View } from 'react-native';
+/* eslint-disable no-undef */
+import { router, useFocusEffect } from 'expo-router';
+import { useCallback } from 'react';
+import { Alert, StatusBar, View } from 'react-native';
 
 import { Button } from '@/components/Button';
 import { HomeHeader } from '@/components/HomeHeader/HomeHeader';
 import { List } from '@/components/List';
 import { Target } from '@/components/Target/Target';
+import { useTargetDatabase } from '@/database/useTargetDatabase';
 
 const summary = {
   total: 'R$ 2.680,00',
@@ -43,6 +46,24 @@ const targets = [
 ];
 
 export default function Index() {
+  const { listBySavedValue } = useTargetDatabase();
+
+  async function fetchTargets() {
+    try {
+      const response = await listBySavedValue();
+      console.log(response);
+    } catch (error) {
+      Alert.alert('Erro', 'Não foi possível carregar as metas');
+      console.log(error);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchTargets();
+    }, [])
+  );
+
   return (
     <View style={{ flex: 1 }}>
       <StatusBar barStyle="light-content" />
