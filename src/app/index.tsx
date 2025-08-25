283e7f6acc61dbc1d6d4949b1e660f3694963b77
Ricardo Baldrez <rdbz@gft.com>
2025-08-25 11:45:31 -0300

    feat: Add func that formats currency

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -11,6 +11,7 @@ import { HomeHeader } from '@/components/HomeHeader';
 import { List } from '@/components/List';
 import { Target, TargetProps } from '@/components/Target';
 import { useTargetDatabase } from '@/database/useTargetDatabase';
+import { numberToCurrency } from '@/utils/numberToCurrency';
 
 const summary = {
   total: 'R$ 2.680,00',
@@ -36,9 +37,9 @@ export default function Index() {
       return response.map(item => ({
         id: String(item.id),
         name: item.name,
-        current: String(item.current),
+        current: numberToCurrency(item.current),
         percentage: item.percentage.toFixed(0) + '%',
-        target: String(item.amount),
+        target: numberToCurrency(item.amount),
       }));
     } catch (error) {
       Alert.alert('Erro', 'Não foi possível carregar as metas');
