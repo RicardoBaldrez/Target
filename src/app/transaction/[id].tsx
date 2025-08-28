d1da7cfccf2a45d5ba23049c0bef6a44255858f0
Ricardo Baldrez <rdbz@gft.com>
2025-08-28 12:35:24 -0300

    feat: Registry transaction in the database

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,8 +1,9 @@
+/* eslint-disable no-undef */
 /* eslint-disable react-native/no-inline-styles */
 
-// import { useLocalSearchParams } from 'expo-router';
+import { router, useLocalSearchParams } from 'expo-router';
 import { useState } from 'react';
-import { View } from 'react-native';
+import { Alert, View } from 'react-native';
 
 import { TransactionTypes } from '../../utils/TransactionTypes';
 
@@ -11,11 +12,44 @@ import { CurrencyInput } from '@/components/CurrencyInput';
 import { Input } from '@/components/Input';
 import { PageHeader } from '@/components/PageHeader';
 import { TransactionType } from '@/components/TransactionType';
+import { useTransactionsDatabase } from '@/database/useTransactionsDatabase';
 
 export default function Transaction() {
+  const [amount, setAmount] = useState(0);
+  const [observation, setObservation] = useState('');
+  const [isCreating, setIsCreating] = useState(false);
   const [selectedType, setSelectedType] = useState(TransactionTypes.Input);
 
-  // const { id } = useLocalSearchParams<{ id: string }>();
+  const { id } = useLocalSearchParams<{ id: string }>();
+  const { create } = useTransactionsDatabase();
+
+  async function handleCreateTransaction() {
+    try {
+      if (amount <= 0) {
+        return Alert.alert('Atenção', 'Preencha o valor da transação');
+      }
+
+      setIsCreating(true);
+
+      await create({
+        target_id: Number(id),
+        amount: selectedType === TransactionTypes.Output ? amount * -1 : amount,
+        observation,
+      });
+
+      Alert.alert('Sucesso!', 'Transação criada com sucesso!', [
+        {
+          text: 'Ok',
+          onPress: () => router.back(),
+        },
+      ]);
+    } catch (error) {
+      console.log('🚀 ~ handleCreateTransaction ~ error:', error);
+      Alert.alert('Erro ao criar transação', 'Tente novamente mais tarde');
+    } finally {
+      setIsCreating(false);
+    }
+  }
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
@@ -25,12 +59,22 @@ export default function Transaction() {
       />
       <View style={{ marginTop: 32, gap: 24 }}>
         <TransactionType selected={selectedType} onChange={setSelectedType} />
-        <CurrencyInput value={0} label="Valor (R$)" />
+        <CurrencyInput
+          value={amount}
+          label="Valor (R$)"
+          onChangeValue={setAmount}
+        />
         <Input
           label="Motivo (opcional)"
           placeholder="Ex: Investir em CDP de 110% no banco XPTO"
+          value={observation}
+          onChangeText={setObservation}
         />
-        <Button title="Salvar" />
+        <Button
+          title="Salvar"
+          isProcessing={isCreating}
+          onPress={handleCreateTransaction}
+        />
       </View>
     </View>
   );
