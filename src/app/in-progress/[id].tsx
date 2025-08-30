7b39684a1908fadb74d8a78d5e0094cbf9cd66c4
Ricardo Baldrez <rdbz@gft.com>
2025-08-30 14:59:28 -0300

    feat: Add formatted date and time

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-undef */
+import dayjs from 'dayjs';
 import { router, useFocusEffect, useLocalSearchParams } from 'expo-router';
 import { useCallback, useState } from 'react';
 import { Alert, View } from 'react-native';
@@ -53,7 +54,7 @@ export default function InProgress() {
           return {
             id: String(item.id),
             value: numberToCurrency(item.amount),
-            date: String(item.created_at),
+            date: dayjs(item.created_at).format('DD/MM/YYYY [às] HH:mm'),
             description: item.observation,
             type:
               item.amount < 0
