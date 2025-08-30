dab78613fc434a24ec2506f0f6bc112ebea78de7
Ricardo Baldrez <rdbz@gft.com>
2025-08-30 15:07:50 -0300

    feat: Add remove transaction method

--- src/database/useTransactionsDatabase.ts
+++ src/database/useTransactionsDatabase.ts
@@ -44,8 +44,13 @@ export function useTransactionsDatabase() {
     `);
   }
 
+  async function remove(id: number) {
+    await database.runAsync(`DELETE FROM transactions WHERE id = ?`, [id]);
+  }
+
   return {
     create,
+    remove,
     listTransactionsByTargetId,
   };
 }
